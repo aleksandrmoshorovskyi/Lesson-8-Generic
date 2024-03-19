50d9fc88eb7b6d5e305ae635c2b7e252cd5a94de
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-03-19 15:58:25 +0200

    UI (Button, Slider, ActivityIndicator)

--- Lesson-8-Generic/ViewController.swift
+++ Lesson-8-Generic/ViewController.swift
@@ -11,43 +11,57 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myBtn: UIButton!
     @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
     
-    
-    
-    @IBAction func MyBtnClicked(_ sender: Any) {
- 
-/*
-        var responseStrings = APIStringResponse()
-        responseStrings.update(with: ["1", "2", "some string"])
-        responseStrings.date = Date()
-        
-        var responseInt = APIIntResponse()
-        responseInt.update(with: [1, 2, 3])
-        responseInt.date = Date()
-*/
-        
-        var responseStrings = APIUniversalResponse<String>()
-        responseStrings.update(with: ["1", "2", "some string"])
-        responseStrings.date = Date()
-        
-        var responseInt = APIUniversalResponse<Int>()
-        responseInt.update(with: [1, 2, 3])
-        responseInt.date = Date()
-        
-        debugPrint(" ... ")
-        
-        var doubleResponse = APIDoubleResponse()
-        doubleResponse.setup(with: [3.0, 5.0])
-        
-        debugPrint(" ... ")
+    @IBAction func mySliderAction(_ sender: UISlider) {
+        print("\(sender.value)")
     }
-   
+    
+    @IBAction func myBtnClicked(_ sender: UIButton) {
+        
+        print("Hello!")
+        
+        doSomeLogicWithData()
+        
+        myActivityIndicator.stopAnimating()
+        
+        print("BB!")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myActivityIndicator.hidesWhenStopped = true
+        myActivityIndicator.startAnimating()
     }
 
 
 }
 
+func doSomeLogicWithData() {
+    
+   /*
+           var responseStrings = APIStringResponse()
+           responseStrings.update(with: ["1", "2", "some string"])
+           responseStrings.date = Date()
+           
+           var responseInt = APIIntResponse()
+           responseInt.update(with: [1, 2, 3])
+           responseInt.date = Date()
+   */
+           
+           var responseStrings = APIUniversalResponse<String>()
+           responseStrings.update(with: ["1", "2", "some string"])
+           responseStrings.date = Date()
+           
+           var responseInt = APIUniversalResponse<Int>()
+           responseInt.update(with: [1, 2, 3])
+           responseInt.date = Date()
+           
+           debugPrint(" ... ")
+           
+           var doubleResponse = APIDoubleResponse()
+           doubleResponse.setup(with: [3.0, 5.0])
+           
+           debugPrint(" ... ")
+}
